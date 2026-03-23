8a79a1d1740bc68e85562803e3fc39258536c33b
Nir Ziv <ziv.nir77@gmail.com>
2026-03-23 10:54:45 -0400

    Chor: change of font style

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        display: ["Space Grotesk", "system-ui", "sans-serif"],
+        display: ["Manrope", "system-ui", "sans-serif"],
         body: ["Plus Jakarta Sans", "system-ui", "sans-serif"],
       },
       colors: {
